86650a68a66a57cd163981f40d72ada38818e552
Piotr Gornik <gornikp94@gmail.com>
2022-12-10 00:31:29 +0100

    clean up

--- Aoc/Day9.cs
+++ Aoc/Day9.cs
@@ -26,28 +26,23 @@ public class Day9 : BaseDay
 
     public record Point(int X, int Y)
     {
-        public Point Move(Point a) => new Point(X + a.X, Y + a.Y);
-        
-        public static Point Create(Direction direction)
+        public Point Move(int x, int y) => new Point(X + x, Y + y);
+        public static Point Create(string direction)
         {
             return direction switch
             {
-                Direction.U => new Point(0, 1),
-                Direction.D => new Point(0, -1),
-                Direction.L => new Point(-1, 0),
-                Direction.R => new Point(1, 0),
+                "U" => new Point(0, 1),
+                "D" => new Point(0, -1),
+                "L" => new Point(-1, 0),
+                "R" => new Point(1, 0),
                 _ => new Point(0, 0)
             };
         }
-
+        
         public Point MoveTail(Point head)
         {
-            if (Math.Abs(head.X - X) > 1 ||  Math.Abs(head.Y - Y) > 1)
-            {
-                int x = Math.Sign(head.X - X);
-                int y = Math.Sign(head.Y - Y);
-                return Move(new Point(x,y));
-            }
+            if (Math.Abs(head.X - X) > 1 ||  Math.Abs(head.Y - Y) > 1) 
+                return Move(Math.Sign(head.X - X), Math.Sign(head.Y - Y));
             return this;
         }
     }
@@ -66,21 +61,18 @@ public class Day9 : BaseDay
 
     private List<Point> SimulateRope(int tailLength)
     {
-        List<Point> tailPoints = new List<Point>();
-        var rope = new List<Point>(tailLength);
-        for (int i = 0; i < tailLength; i++)
-        {
-            rope.Add(new Point(0, 0));
-        }
+        var tailPoints = new List<Point>();
+        var rope = Enumerable.Range(0, tailLength).Select(x => new Point(0, 0)).ToList();
 
         foreach (var moveString in _data)
         {
             var splitted = moveString.Split(" ", StringSplitOptions.TrimEntries);
-            var move = Point.Create(Enum.Parse<Direction>(splitted[0]));
+            var move = Point.Create(splitted[0]);
             var numberOfMoves = int.Parse(splitted[1]);
+            
             for (int i = 0; i < numberOfMoves; i++)
             {
-                rope[0] = rope[0].Move(move);
+                rope[0] = rope[0].Move(move.X, move.Y);
 
                 for (int j = 1; j < rope.Count; j++)
                 {
@@ -93,13 +85,5 @@ public class Day9 : BaseDay
 
         var distinctPoints = tailPoints.Distinct().ToList();
         return distinctPoints;
-    }
-
-    public enum Direction
-    {
-        U = 1,
-        D = 2,
-        L = 3,
-        R = 4,
     }
 }
